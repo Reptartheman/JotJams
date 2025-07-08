a4587bb075eeac07464e555b995dfde632dc710f
Jeff Moro <jeffmoromusic@gmail.com>
2025-07-08 14:25:42 -0700

    add sort logic

--- scripts/favs.js
+++ scripts/favs.js
@@ -2,14 +2,13 @@ import { createElementUtil } from "./utils.js";
 
 const favoritesList = document.getElementById("favoritesList");
 const backToSearch = document.getElementById("backToSearch");
-console.log(favoritesList);
+const sortBtns = document.querySelectorAll(".sortBtn");
 backToSearch.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
 const renderFavorites = () => {
   const favorites = JSON.parse(localStorage.getItem("favs")) || [];
-  console.log(favorites);
   if (favorites.length === 0) {
     const emptyMsg = createElementUtil("li");
     emptyMsg.textContent = "No favorites yet. Go add some!";
@@ -22,6 +21,8 @@ const renderFavorites = () => {
     const li = createElementUtil("li");
     const thumbNail = createElementUtil("img");
     li.classList.add("fav");
+    li.id = item.title;
+    li.setAttribute('data-title', item.title);
     li.textContent = `${index + 1}. ${item.title}`;
     thumbNail.classList.add("favImg");
     thumbNail.src = `${item.cover_image}`
@@ -41,5 +42,36 @@ const renderFavorites = () => {
     favoritesList.appendChild(li);
   });
 };
+
+function updateFavoriteNumbers() {
+  const items = favoritesList.querySelectorAll(".fav");
+  items.forEach((item, idx) => {
+    item.childNodes[0].textContent = `${idx + 1}. ${item.getAttribute('data-title')}`;
+  });
+}
+
+
+function sortFavorites(direction) {
+  const favorites = favoritesList.querySelectorAll(".fav");
+  const favoritesArray = Array.from(favorites);
+
+  favoritesArray.sort((a, b) => {
+    if (direction === 'asc') {
+      return a.id.localeCompare(b.id);
+    } else {
+      return b.id.localeCompare(a.id);
+    }
+  });
+
+  favoritesArray.forEach(item => favoritesList.appendChild(item));
+  updateFavoriteNumbers();
+}
+
+sortBtns.forEach(button => {
+  button.addEventListener('click', (e) => {
+    const direction = e.target.dataset.sortdir;
+    sortFavorites(direction);
+  });
+});
 
 renderFavorites();
